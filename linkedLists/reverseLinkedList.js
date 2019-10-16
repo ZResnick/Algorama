6760ac3611d7b8aa2610446c6b67006c8d0cfc3d
Zach Resnick <zachary.l.resnick@gmail.com>
2019-10-16 17:25:09 -0400

    Reverse Linked List - Iterative

---
+++ linkedLists/reverseLinkedList.js
@@ -0,0 +1,45 @@
+/*
+Reverse a singly linked list and return the new head
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+
+Output: 5->4->3->2->1->NULL
+
+
+
+
+1, 2, 3, 4, 5
+2, 1,    3, 4, 5
+3, 2, 1   4, 5
+*/
+
+var reverseList = function(head) {
+  let temp = [];
+  while (head) {
+    temp.push(head.val);
+    head = head.next;
+  }
+
+  let node = {};
+  let newHead = null;
+  for (let i = 0; i < temp.length; i++) {
+    let curEl = temp[i];
+    node.val = curEl;
+    node.next = newHead;
+    newHead = node;
+    node = {};
+  }
+  return newHead;
+};
+
+const input = {
+  val: 1,
+  next: {
+    val: 2,
+    next: { val: 3, next: { val: 4, next: { val: 5, next: null } } },
+  },
+};
+
+console.log(reverseList(input));
