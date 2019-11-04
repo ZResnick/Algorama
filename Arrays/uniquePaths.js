a5538c3f5a282d338d5406aa7fcabdabce39a35b
Zach Resnick <zachary.l.resnick@gmail.com>
2019-11-04 13:07:07 -0500

    Adds Unique Paths

---
+++ Arrays/uniquePaths.js
@@ -0,0 +1,28 @@
+/*
+
+A robot is located at the top-left corner of a m x n grid
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid.
+
+How many possible unique paths are there?
+
+*/
+
+const uniquePaths = (rows, cols) => {
+  // The Below works too bu doens tlook as nice.
+  // let grid = Array(rows).fill(Array(cols).fill(1));
+
+  let grid = Array(rows);
+  for (let rowNum = 0; rowNum < rows; rowNum++) {
+    grid[rowNum] = Array(cols).fill(1);
+  }
+  for (let row = 1; row < rows; row++) {
+    for (let col = 1; col < cols; col++) {
+      grid[row][col] = grid[row - 1][col] + grid[row][col - 1];
+    }
+  }
+  console.log(grid);
+  return grid[rows - 1][cols - 1];
+};
+
+console.log(uniquePaths(10, 10));
--- README.md
+++ README.md
@@ -37,6 +37,7 @@ This repo is built for algorithm problems and practice, eith thre main goal bein
 - n Queens (H) _ Not yet coompleted _
 - Best Time to Buy and Sell Stocks (E)
 - Find the Duplicate Number (M)
+- Unique Paths (M)
 
 ### Binary Trees
 
